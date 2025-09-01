5394deef84fea9e9f36460900a4259fa67363b30
pearyl <131762349+pearyl@users.noreply.github.com>
2025-09-01 11:50:00 +0530

    Update string_calculator.py

--- string_calculator.py
+++ string_calculator.py
@@ -3,7 +3,14 @@ import re
 def split_numbers(input_string):
   if not input_string:
     return []
-  return re.split(r'[,\n]', input_string)
+  delimiters = [',','\n']
+  if input_string.startswith("//"):
+    parts = input_string.split("\n", 1)
+    custom_delim, input_string = parts[0], parts[1]
+    delimiters.append(custom_delim)
+  pattern = "|".join(map(re.escape, delimiters))
+  print(pattern)
+  return re.split(pattern, input_string)
 
 def sum(input_string):
   numbers = split_numbers(input_string)
